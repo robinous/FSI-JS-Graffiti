74b5bfe8052f69c0a1fd482cc3575e6060e07e14
robinous <ousephrobin9@gmail.com>
2022-06-29 21:05:02 -0400

    Dom activity

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is an activity for FSWD Lesson 1.11.1- The Document Object Model.
 
-Please refer to the Activity Guide in Canvas for directions.
+This is a dom practice activity we did
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Select some elements...
+
+let header= document .querySelector('#page-header')
+header .style.textAlign= 'left'
+
+let dogImages= document.querySelectorAll('.dog-Image')
+for(let i = 0; i < dogImages.length; i++){
+dogImages[i].style.textalign = "left"
+}
+
+let darkFooter = document.querySelector('.footer')
+darkFooter.style.color = "purple"
+
+let footer =document.querySelector('.footer')
+footer.style.color = 'blue'
+footer.style.border = 'solid 2px black'
+footer.style.backgroundColor = 'red'
+
+
+
+
